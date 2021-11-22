2713ba615cafc17113d8cd9e2de9eab3ae99642c
Leonardo Vogel <leeo.vog13@gmail.com>
2021-11-21 22:11:44 -0300

    Exercício 1 - Bloco 5.2

---
+++ 01_fundamentos/b05_javascript-dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,4 @@
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+const elementH1 = document.createElement('h1');
+elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(elementH1);
